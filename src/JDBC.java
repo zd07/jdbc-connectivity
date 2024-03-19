acfb4718777e8384a40d63ab54b5e1b8f4face77
zd07 <zahidibnrizwan@gmail.com>
2024-03-20 01:29:18 +0530

    JDBC connection

---
+++ src/JDBC.java
@@ -0,0 +1,51 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class JDBC {
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/blog_api";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "zd@mysql";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter username:");
+        String name = scanner.nextLine();
+
+        System.out.println("Enter password:");
+        String password = scanner.nextLine();
+
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+            String query = "SELECT * FROM user WHERE name = ? AND password = ?";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, name);
+            statement.setString(2, password);
+            ResultSet resultSet = statement.executeQuery();
+
+            // Check if user exists
+            if (resultSet.next()) {
+                System.out.println("Login successful. Welcome, " + resultSet.getString("email") +  "!");
+            } else {
+                System.out.println("User not found.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            scanner.close();
+        }
+    }
+}
